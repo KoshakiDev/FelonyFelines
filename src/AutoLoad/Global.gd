00675e335a4d1471b87cba06770bacb77c34e139
Mansur <funisinfinity26@gmail.com>
2022-01-02 18:33:17 +0600

    boid and keep distance logic for enemies

--- src/AutoLoad/Global.gd
+++ src/AutoLoad/Global.gd
@@ -23,3 +23,23 @@ func reparent(child: Node, new_parent: Node):
 	var old_parent = child.get_parent()
 	old_parent.remove_child(child)
 	new_parent.add_child(child)
+
+func get_all_enemies():
+	var enemies = {
+		"scissors":[],
+		"rock":[],
+		"imp":[]
+	}
+	
+	var enemy_types = enemies.keys()
+	
+	for spawner in main.spawners:
+		var result = []
+		var entities = spawner.parent.get_children()
+		for entity in entities:
+			# Checker if its an enemy, if true then add it to result
+			if("type" in entity):
+				if(entity.type in enemy_types):
+					enemies[entity.type].append(entity)
+	
+	return enemies
--- src/entities/enemies/states/Chase.gd
+++ src/entities/enemies/states/Chase.gd
@@ -3,11 +3,85 @@ extends State
 func enter(msg := {}) -> void:
 	owner.play_animation("Run", "Animations")
 
-func seek_steering(vector_to_target: Vector2) -> Vector2:
-	var desired_velocity: Vector2 = vector_to_target.normalized() * owner.max_speed
+func seek_steering(desired_direction_vector: Vector2) -> Vector2:
+	var desired_velocity: Vector2 = desired_direction_vector.normalized() * owner.max_speed
 	return desired_velocity - owner.velocity
+
+func ent_dist(entity1, entity2):
+	return (entity1.global_position - entity2.global_position).length()
+
+func closest_target(targets: Array):
+	var min_id = 0
+	for i in range(len(targets)):
+		if(ent_dist(owner, targets[i]) < ent_dist(owner, targets[min_id])):
+			min_id = i
+	return targets[min_id]
+
+
+func boid_cohesion(entities: Array):
+	var neighborhood_distance = 300
+	var sum: Vector2 = Vector2.ZERO
+	var count = 0
 	
+	for entity in entities:
+		var d = ent_dist(owner, entity)
+		if(d > 0 and d < neighborhood_distance):
+			sum += entity.global_position
+			count += 1
+	if(count > 0):
+		sum /= count
+		
+		var steer = sum - owner.global_position
+		return steer
+	else:
+		return Vector2.ZERO
+
+func boid_alignment(entities: Array):
+	var neighborhood_distance = 300
+	var sum: Vector2 = Vector2.ZERO
+	var count = 0
+	
+	for entity in entities:
+		var d = ent_dist(owner, entity)
+		if(d > 0 and d < neighborhood_distance):
+			sum += entity.velocity
+			count += 1
+	
+	if(count > 0):
+		sum /= count
+		sum = sum.normalized()
+		sum *= owner.max_speed
+		var steer = sum - owner.velocity
+		return steer
+	else:
+		return Vector2.ZERO
+
+func boid_separate(entities: Array):
+	var desired_separation = 50
+	var steer: Vector2 = Vector2.ZERO
+	var count = 0
+	for entity in entities:
+		var d = ent_dist(owner, entity)
+		if(d > 0 and d < desired_separation):
+			var diff = owner.global_position - entity.global_position
+			diff = diff.normalized()
+			diff /= d
+			steer += diff
+			count += 1
+	if(count > 0):
+		steer /= count
+		
+	if(steer.length() > 0):
+		steer = steer.normalized()
+		steer *= owner.max_speed
+		steer -= owner.velocity
+	
+	return steer
+
 func physics_update(delta: float) -> void:
+	
+	var similar_enemies = Global.get_all_enemies()[owner.type]
+	
 	if owner.is_dead():
 		state_machine.transition_to("Death")
 	var targets = owner.find_targets_in_area(["player"], owner.vision_area)
@@ -15,11 +89,27 @@ func physics_update(delta: float) -> void:
 		state_machine.transition_to("Idle")
 		return
 		
-	var target = targets[0]
+	
+	var target = closest_target(targets)
+	var total_vector
 	
 	var vector_to_target = target.global_position - owner.global_position
+
+	if(owner.type == "imp" and len(similar_enemies) > 1):
+		var c = boid_cohesion(similar_enemies)
+		var a = boid_alignment(similar_enemies)
+		var s = boid_separate(similar_enemies)
+		
+		total_vector = vector_to_target * .3 + c * .2 + a * .2 + s * .3
+	elif(owner.type == "rock"):
+		if(ent_dist(owner, target) < 50):
+			total_vector = -vector_to_target
+		else:
+			total_vector = vector_to_target
+	else:
+		total_vector = vector_to_target
 	
-	var steering: Vector2 = seek_steering(vector_to_target)
+	var steering: Vector2 = seek_steering(total_vector)
 	steering = steering.clamped(owner.max_steering)
 	
 	owner.velocity = owner.velocity + steering
