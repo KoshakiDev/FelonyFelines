aff8cb522f063723b3fea5a109b1ff2f88d5b00f
Xanthos Ware <xanthosgardenstudios@gmail.com>
2022-01-22 17:34:46 +0600

    small fixes and healthbar

--- src/entities/items/weapons/range/assault rifle/AssaultRifle.gd
+++ src/entities/items/weapons/range/assault rifle/AssaultRifle.gd
@@ -1,8 +1,8 @@
 extends "res://src/entities/items/weapons/range/rangeModule.gd"
 
-onready var animation_player := $AnimationPlayer
-onready var bullet_spawner = $BulletSpawner
-onready var despawn_timer := $DespawnTimer
+onready var animation_player := $Position2D/AnimationPlayer
+onready var bullet_spawner = $Position2D/BulletSpawner
+onready var despawn_timer := $Position2D/DespawnTimer
 
 func _ready():
 	setup_despawn()
