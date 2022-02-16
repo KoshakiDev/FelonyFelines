c90a276f8c44866806aa99d7e9a50044cc488433
Mansur <funisinfinity26@gmail.com>
2022-02-16 15:41:51 +0100

    ✨ NPC chooses a weapon

--- src/entities/players/npc/states/Attack.gd
+++ src/entities/players/npc/states/Attack.gd
@@ -2,14 +2,29 @@ extends State
 
 #NPC ATTACK STATE
 
+var close_range_weapons = ["FISTS", "AXE", "SPEAR", "SHIELD"]
+var long_range_weapons = ["REVOLVER", "SHOTGUN", "ASSAULTRIFEL", "MINIGUN"]
+
+func chooseWeapon():
+	var enemy_position = Global.get_closest_enemy(owner.global_position)
+	var distance = enemy_position.distance_to(owner.global_position)
+	
+	if distance > 100:
+		while not owner.weapon_manager.cur_weapon.entity_name in long_range_weapons:			
+			owner.switch_to_next_weapon()
+	else:
+		while not owner.weapon_manager.cur_weapon.entity_name in close_range_weapons:
+			owner.switch_to_next_weapon()
+
 func enter(msg := {}) -> void:
 	owner.play_animation("Idle", "Movement")
+	chooseWeapon()
 	owner.action()
 	state_machine.transition_to("Move")
 
 func exit() -> void:
 	pass
-
+	
 
 
 #don't use this not finished
